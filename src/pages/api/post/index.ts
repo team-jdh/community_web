4599e3f285a174dd1f657675de1253c6b58c2dc3
devsince2021 <dev.ryu2021@gmail.com>
2024-01-24 11:31:31 +0900

    삭제 api 구현

---
+++ src/pages/api/post/index.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "POST":
+      return handlePost(req, res);
+    case "DELETE":
+      return handleDelete(req, res);
+    default:
+      res.setHeader("Allow", ["POST", "DELETE"]);
+      return res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
+
+const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
+  const apiUrl = `${process.env.API_BASE_URL}/post`;
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: req.body,
+  });
+
+  const isSuccess = response.status === 201;
+
+  res.status(response.status).json({ isSuccess });
+};
+
+const handleDelete = async (req: NextApiRequest, res: NextApiResponse) => {
+  const apiUrl = `${process.env.API_BASE_URL}/post`;
+  const response = await fetch(apiUrl, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: req.body,
+  });
+
+  const isSuccess = response.status === 204;
+
+  res.status(response.status).json({ isSuccess });
+};
